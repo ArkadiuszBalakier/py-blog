522a0e41d60bea25ea197dd3a64ab049d070b3c5
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-01-07 11:11:59 +0100

    creating indext.html

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from blog.models import Post
 
 # Create your views here.
 def index(request):
-    post_list = Post.objects.all().order_by('-created_at')
+    post_list = Post.objects.all().order_by('-created_time')
     context = {
         "post_list": post_list,
     }
